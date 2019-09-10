5a996d3bbb3d84ace964555c386e2339c145628f
Nicholas Mascia <nmmascia@gmail.com>
2019-09-10 00:33:52 -0400

    Add counter for users

--- src/response/handler.js
+++ src/response/handler.js
@@ -1,24 +1,51 @@
 const querystring = require('querystring');
 const fetch = require('node-fetch');
+const AWS = require('aws-sdk');
+const { startOfISOWeek, format } = require('date-fns');
 
-const { BOT_TOKEN } = process.env;
+const { BOT_TOKEN, WORKOUTS_DB_TABLE } = process.env;
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-  console.log('EVENT:', event);
   const { payload } = querystring.parse(event.body);
   const data = JSON.parse(payload);
+  console.log('input data:', data);
+
   const [action] = data.actions;
   const responseUrl = data.response_url.replace('\\\\', '');
 
+  const storageKey = format(startOfISOWeek(new Date()), 'MMddyyyy');
+  const { user } = data;
+  console.log('storage key', storageKey);
+
   switch (action.name) {
     case 'TALLY_WORKOUT': {
+      const params = {
+        TableName: WORKOUTS_DB_TABLE,
+        Key: {
+          week: storageKey,
+        },
+        UpdateExpression: 'add #user :val',
+        ExpressionAttributeNames: {
+          '#user': user.id,
+        },
+        ExpressionAttributeValues: {
+          ':val': 1,
+        },
+        ReturnValues: 'UPDATED_NEW',
+      };
+
+      const { Attributes } = await dynamoDb.update(params).promise();
+      console.log('[dynamodb.update]', Attributes);
+
       const result = await fetch(responseUrl, {
         method: 'POST',
         body: JSON.stringify({
           replace_original: true,
           attachments: [
             {
-              text: 'Good work, you worked out one time this week!',
+              text: `Good work <@${user.id}>! Here's your total workout count this week: ${Attributes[user.id]}`,
               title: 'Your workout has been tallied!',
             },
           ],
