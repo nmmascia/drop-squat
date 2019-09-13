d2194aee5f73670368405eda727abd283e67e41a
Nicholas Mascia <nmmascia@gmail.com>
2019-09-12 21:08:58 -0400

    Get leaderboard rendering

--- src/response/handler.js
+++ src/response/handler.js
@@ -10,14 +10,13 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 exports.handler = async (event) => {
   const { payload } = querystring.parse(event.body);
   const data = JSON.parse(payload);
-  console.log('input data:', data);
-
   const [action] = data.actions;
   const responseUrl = data.response_url.replace('\\\\', '');
-
-  const storageKey = format(startOfISOWeek(new Date()), 'MMddyyyy');
   const { user } = data;
-  console.log('storage key', storageKey);
+  const date = startOfISOWeek(new Date());
+  const storageKey = format(date, 'MMddyyyy');
+  const userCountKey = `count_${user.id}`;
+  console.log('EVENT', action.name, storageKey, user.id);
 
   switch (action.name) {
     case 'TALLY_WORKOUT': {
@@ -26,30 +25,90 @@ exports.handler = async (event) => {
         Key: {
           week: storageKey,
         },
-        UpdateExpression: 'add #user :val',
+        UpdateExpression: 'add #user_count :val',
         ExpressionAttributeNames: {
-          '#user': user.id,
+          '#user_count': userCountKey,
         },
         ExpressionAttributeValues: {
           ':val': 1,
         },
         ReturnValues: 'UPDATED_NEW',
       };
-
       const { Attributes } = await dynamoDb.update(params).promise();
-      console.log('[dynamodb.update]', Attributes);
-
       const attachments = [
         {
-          text: `Good work <@${user.id}>! Here's your total workout count this week: ${Attributes[user.id]}`,
+          text: `Good work <@${user.id}>! Here's your total workout count this week: ${Attributes[userCountKey]}`,
           title: 'Your workout has been tallied!',
         },
       ];
-      const json = await chatUpdateAPI({ responseUrl, body: { attachments } });
-      console.log('[response.handle] status code', json.status);
+      await chatUpdateAPI({ responseUrl, body: { attachments } });
     }
     case 'GET_WEEKLY_LEADERBOARD': {
-      console.log('tbd...');
+      const { Item } = await dynamoDb
+        .get({
+          TableName: WORKOUTS_DB_TABLE,
+          Key: {
+            week: storageKey,
+          },
+        })
+        .promise();
+
+      const counts = Object.entries(Item)
+        .reduce((acc, [key, count]) => {
+          const [prefix, userId] = key.split('_');
+          if (prefix === 'count') {
+            acc.push({ userId, count });
+          }
+          return acc;
+        }, [])
+        .sort(({ count: a }, { count: b }) => b - a)
+        .map(({ userId, count }, index) => {
+          return {
+            type: 'section',
+            fields: [
+              {
+                type: 'mrkdwn',
+                text: `:first_place_medal: <@${userId}>`,
+              },
+              {
+                type: 'plain_text',
+                text: `${count}`,
+              },
+            ],
+          };
+        });
+
+      const blocks = [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `*Leaderboard for ${format(date, 'MMM do, yyyy')}*`,
+          },
+        },
+        {
+          type: 'section',
+          fields: [
+            {
+              type: 'mrkdwn',
+              text: '*Squatter*',
+            },
+            {
+              type: 'mrkdwn',
+              text: '*Workouts*',
+            },
+          ],
+        },
+        ...counts,
+      ];
+
+      console.log('Created blocks', blocks);
+      try {
+        const json = await chatUpdateAPI({ responseUrl, body: { blocks } });
+        console.log('result', json);
+      } catch (err) {
+        console.log('error', err);
+      }
     }
     default: {
       return {
